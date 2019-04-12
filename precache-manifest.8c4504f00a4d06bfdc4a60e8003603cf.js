23bdf78a3040a32a2bcf0962a779af9f29c42542
yongsk0066 <yongsk0066@naver.com>
2019-04-12 19:21:41 +0900

    Updates

---
+++ precache-manifest.8c4504f00a4d06bfdc4a60e8003603cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b126fa9245bc9a3ec851",
+    "url": "/react_practice-movie-/static/css/main.95ac0dd8.chunk.css"
+  },
+  {
+    "revision": "b126fa9245bc9a3ec851",
+    "url": "/react_practice-movie-/static/js/main.a512f756.chunk.js"
+  },
+  {
+    "revision": "05c123e815a01a139d27",
+    "url": "/react_practice-movie-/static/js/runtime~main.aa17d248.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/react_practice-movie-/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "e8b1dd7097953f20b1f3868a6d17d2e5",
+    "url": "/react_practice-movie-/index.html"
+  }
+];
